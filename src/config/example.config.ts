045fcae6fb87f2539e4683c40acf5f3a152e91d8
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2021-08-20 17:53:26 +0100

    🎉add inital project starter files

---
+++ src/config/example.config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "YOUR_AWS_RDS_USERNAME",
+    "password": "YOUR_AWS_RDS_PASSWORD",
+    "database": "YOUR_AWS_RDS_DBNAME",
+    "host": "YOUR_AWS_RDS_HOST",
+    "dialect": "postgres",
+    "aws_region": "us-east-2",
+    "aws_profile": "default",
+    "aws_media_bucket": "YOUR_AWS_S3_BUCKET"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
